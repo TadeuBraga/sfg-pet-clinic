d74c5213be1fe83cca2f19bc0d118200d9f735fb
Tadeu Rodrigues <tadeufala@gmail.com>
2019-09-14 16:51:41 -0300

    Intial Project Add

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package br.com.tadeu.gurucourse.sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.tadeu.gurucourse.sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
