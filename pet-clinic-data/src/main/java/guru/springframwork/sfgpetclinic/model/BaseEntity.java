d0c6c0d3ff87974b125baddf891b9da6e7defc6e
Tadeu Rodrigues <tadeufala@gmail.com>
2019-12-08 11:21:33 -0300

    Adding base entity. Closes #10

---
+++ pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package guru.springframwork.sfgpetclinic.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
